b53c4e438dc72265048a4e5ec0ec7fe6c81a505c
andrespxp <davidmonterrosa52@gmail.com>
2021-05-28 14:39:39 -0500

    crud de notas actualizado

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 include_once("./include/header.php");
 ?>
 
-<p>  efrbgwehlsbtueuibveutblptionts dhbfavklehyl fe bfvaih eclasd 
-  eutbh´pwetnhb´setibnet esntb t b wptp bg ´pentp bpej psr b .eu8tr v9qwbrf 
- euib oasyfo seoutbnpsuilo sbañdjkvNÁWUIBV´SDBN{ SDIFB+AEOMRBETPB,K  JBN ZBVFA
-ALUBFVÁSNDF´NAP PAN LADFV  PERTHOPN  ÑSDJF VLEIWGF´webv clñ} </p>
+
 
 <?php
 session_start();
@@ -30,7 +27,7 @@ if (isset($_POST['btnLogin'])) {
         exit;
     }
 }
-
+?>
 <?php
 include_once("./include/footer.php");
 ?>
